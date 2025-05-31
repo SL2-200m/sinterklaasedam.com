c2c220981cde5c2455b6b991256ecf48b90f0412
SL2Musaraf <sl2@200m.co>
2025-05-31 08:50:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+// This site currently has no specific JavaScript functionality.
+// This file is included to fulfill the request for HTML, CSS, and JS.
+console.log("Sinterklaas Edam site script loaded.");
